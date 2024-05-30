670255f3703403211fe5db3e9f3e3ec7be0edf39
Gian-Andrea Hehli <gian-andrea.hehli@slf.ch>
2024-05-30 07:53:36 +0200

    tried to get correct positioning from sand plus but didn't succeed yet. might have to go back to negative sand where it worked before

--- fitPointcloud.py
+++ fitPointcloud.py
@@ -1,6 +1,7 @@
 # IMPORT PACKAGES AND MODULES
 import cv2
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from pyntcloud import PyntCloud
 from scipy.spatial.transform import Rotation as R
@@ -47,32 +48,34 @@ def calculate_transformation_matrix(points_3d_source, points_3d_target):
 
 
 
-
-pattern = 100 * np.array([[1.06, 0.82, 0], [9.15, 0.82, 0], [9.12, 8.92, 0], [1.05, 8.92, 0],
+# Define pattern as 3D coordinates in mm
+pattern = 10 * np.array([[1.06, 0.82, 0], [9.15, 0.82, 0], [9.12, 8.92, 0], [1.05, 8.92, 0],
                           [50.7, 0.91, 0], [58.77, 0.94, 0], [58.75, 9.04, 0], [50.66, 9, 0],
                           [50.57, 51.09, 0], [58.63, 50.99, 0], [58.73, 59.07, 0], [50.66, 59.14, 0],
                           [1.08, 51.02, 0], [9.15, 51.02, 0], [9.12, 59.13, 0], [1.04, 59.11, 0]])
 
 # Load the .ply file
-cloud = PyntCloud.from_file('H:/data/cloudcompare/test/final.ply')
+cloud = PyntCloud.from_file('H:/data/cloudcompare/test/sand_plus.ply')
 
 # Translate the point cloud so all 'x' and 'y' values are positive
 min_x = cloud.points['x'].min()
-min_y = cloud.points['y'].min()
+max_y = cloud.points['y'].max()
 cloud.points['x'] -= min_x
-cloud.points['y'] -= min_y
+cloud.points['y'] -= max_y
 
 print('Point cloud size: ', cloud.points.shape)
+print(f'Min x: {min_x}, Max y: {max_y}')
+print(f'Max x: {cloud.points["x"].max()}, Min y: {-cloud.points["y"].min()}')
 
 # Create an empty list to store the indices
-indices = np.empty((int(cloud.points['y'].max()) + 1, int(cloud.points['x'].max()) + 1), dtype=object)
+indices = np.empty((int(-cloud.points['y'].min()) + 1, int(cloud.points['x'].max()) + 1), dtype=object)
 
 # Now create the image and store the indices
-image = np.zeros((int(cloud.points['y'].max()) + 1, int(cloud.points['x'].max()) + 1, 3), dtype=np.uint8)
+image = np.zeros((int(-cloud.points['y'].min()) + 1, int(cloud.points['x'].max()) + 1, 3), dtype=np.uint8)
 for index, point in cloud.points.iterrows():
-    x, y = int(point['x']), int(point['y'])
-    image[y, x] = [point['blue'], point['green'], point['red']]
-    indices[y, x] = index
+    x_img, y_img = int(point['x']), -int(point['y'])
+    image[y_img, x_img] = [point['blue'], point['green'], point['red']]
+    indices[y_img, x_img] = index
 
 print('Image shape: ', image.shape)
 
@@ -118,6 +121,9 @@ print(f'Mean of all errors: {distances_min.mean()}')
 print(f'Source shape: {src.shape}')
 print(f'Pattern shape: {pattern.shape}')
 
+print(f'Source: {src}')
+print(f'Pattern: {pattern}')
+
 # Initialize the transformation matrix to the identity matrix
 M = np.eye(4)
 
@@ -137,11 +143,9 @@ for i in range(max_iterations):
 
     # Calculate the new transformation that best aligns the transformed source points to the closest target points
     M_new = calculate_transformation_matrix(src_transformed, pattern[indices.flatten()])
-    print('M_new: ', M_new)
 
     # Update the transformation matrix
     M = np.dot(M_new, M)
-    print('M: ', M)
 
     # Check for convergence
     if np.linalg.norm(M_new - np.eye(4)) < convergence_threshold:
@@ -149,16 +153,28 @@ for i in range(max_iterations):
 
 print('Transformation matrix: ', M)
 
+# Create an empty DataFrame with the same columns as the original point cloud's points
+df_new = pd.DataFrame(columns=cloud.points.columns)
+
 # Apply the transformation matrix to the point cloud
-cloud.points['x'], cloud.points['y'], cloud.points['z'], _ = np.dot(M, np.array([cloud.points['x'], cloud.points['y'], cloud.points['z'], np.ones(cloud.points.shape[0])]))
+x, y, z, _ = np.dot(M, np.array([cloud.points['x'], cloud.points['y'], cloud.points['z'], np.ones(cloud.points.shape[0])]))
 
+# Fill the new point cloud with the transformed coordinates in higher resolution (0.1mm)
+df_new['x'] = 10*x
+df_new['y'] = 10*y
+df_new['z'] = 10*z
+df_new['red'] = cloud.points['red']
+df_new['green'] = cloud.points['green']
+df_new['blue'] = cloud.points['blue']
 
+# Create a new point cloud with the new DataFrame
+cloud_new = PyntCloud(df_new)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.view_init(elev=0, azim=-90, roll=0)
 
-ax.scatter(cloud.points['x'], cloud.points['y'], cloud.points['z'])
+ax.scatter(cloud_new.points['x'], cloud_new.points['y'], cloud_new.points['z'])
 
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
@@ -166,23 +182,23 @@ ax.set_zlabel('Z')
 
 plt.show()
 
-# Translate the point cloud so all 'x' and 'y' values are positive
+""" # Translate the point cloud so all 'x' and 'y' values are positive
 min_x = cloud.points['x'].min()
 min_y = cloud.points['y'].min()
 cloud.points['x'] -= min_x
-cloud.points['y'] -= min_y
+cloud.points['y'] -= min_y """
 
-print('Point cloud size: ', cloud.points.shape)
+print('Point cloud size: ', cloud_new.points.shape)
 
 # Create an empty list to store the indices
-indices = np.empty((int(cloud.points['y'].max()) + 1, int(cloud.points['x'].max()) + 1), dtype=object)
+indices = np.empty((int(-cloud_new.points['y'].min()) + 1, int(cloud_new.points['x'].max()) + 1), dtype=object)
 
 # Now create the image and store the indices
-image = np.zeros((int(cloud.points['y'].max()) + 1, int(cloud.points['x'].max()) + 1, 3), dtype=np.uint8)
-for index, point in cloud.points.iterrows():
-    x, y = int(point['x']), int(point['y'])
-    image[y, x] = [point['blue'], point['green'], point['red']]
-    indices[y, x] = index
+image = np.zeros((int(-cloud_new.points['y'].min()) + 1, int(cloud_new.points['x'].max()) + 1, 3), dtype=np.uint8)
+for index, point in cloud_new.points.iterrows():
+    x_img, y_img = int(point['x']), -int(point['y'])
+    image[y_img, x_img] = [point['blue'], point['green'], point['red']]
+    indices[y_img, x_img] = index
 
 print('Image shape: ', image.shape)
 
